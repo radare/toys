62855cc1309d1c5193a245cf472c19f844108c92
pancake <pancake@nopcode.org>
2009-10-28 11:54:40 +0100

    * Added jarpath utility * Fix minor warnings in libwipe

---
+++ jarpath.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+JARPATH="/usr/pkg/share/classpath:/usr/pkg/lib/java"
+JARPATH=`echo $JARPATH | awk '{ gsub(/:/," "); print $0}'`
+
+for A in $JARPATH ; do
+  for B in `ls $A/*.jar 2> /dev/null` ; do
+    CLASSPATH=$B:$CLASSPATH
+  done
+done
+echo $CLASSPATH

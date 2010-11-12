b8fe69c7a5de457536038585395324a0699c419f
pancake <pancake@nopcode.org>
2010-11-12 17:33:45 +0100

    * Add support for board selection in 4chan.sh

--- 4chan/4chan.sh
+++ 4chan/4chan.sh
@@ -5,6 +5,7 @@ if [ "$1" = "-h" ]; then
   echo "while : ; do ./4chan.sh -c ; sleep 60 ; done"
   exit 0
 fi
+b=b; [ -n "$BOARD" ] && b=$BOARD
 if [ "$1" = "-c" ]; then
   CUR=$(ls *.jpg *.png *.gif)
 #  rm -f *.jpg *.png *.gif
@@ -15,15 +16,16 @@ fi
 [ -z "$(pidof eog)" ] && eog -s -f . &
 num=$1
 [ -z "$num" ] && num=0
-echo "[4ch] fetching /b/.."
-curl -s curl http://boards.4chan.org/b/ > a
+echo "[4ch] fetching /$b/.."
+curl -s curl http://boards.4chan.org/$b/ > a
 for a in $(seq 1 $num) ; do
-	curl -s curl http://boards.4chan.org/b/$a >> a
+	curl -s curl http://boards.4chan.org/$b/$a >> a
 done
 cat a | grep -e jpg -e gif -e png | grep images.4chan | \
 	perl -ne 'if (/href="([^"]*)"/) { $p=$1; if ($1=~/http/) {print "$p\n"}}' > b
 echo "[4ch] downloading pics.."
 for a in $(cat b); do
+echo $a
   f=$(basename $a)
   if [ ! -f $a ]; then
     echo " * $a"
@@ -36,3 +38,4 @@ for a in $(cat b); do
   fi
 done
 [ -n "$CUR" ] && rm -f ${CUR}
+echo "[4ch] done"

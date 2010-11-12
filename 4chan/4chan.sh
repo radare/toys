c2ec902c2316bdb2f5774e7c90f562f1037a4d25
pancake <pancake@nopcode.org>
2010-11-12 04:36:00 +0100

    * Use eog and check for downloaded files in local (faster)

--- 4chan/4chan.sh
+++ 4chan/4chan.sh
@@ -8,13 +8,12 @@ fi
 if [ "$1" = "-c" ]; then
   rm -f *.jpg *.png *.gif
   shift
-else
-  eog . & #gqview &
 fi
+[ -z "$(pidof eog)" ] && eog -s -f . &
 num=$1
 [ -z "$num" ] && num=0
 echo "[4ch] fetching /b/.."
-curl -s curl http://boards.4chan.org/b/> a
+curl -s curl http://boards.4chan.org/b/ > a
 for a in $(seq 1 $num) ; do
 	curl -s curl http://boards.4chan.org/b/$a >> a
 done
@@ -22,7 +21,14 @@ cat a | grep -e jpg -e gif -e png | grep images.4chan | \
 	perl -ne 'if (/href="([^"]*)"/) { $p=$1; if ($1=~/http/) {print "$p\n"}}' > b
 echo "[4ch] downloading pics.."
 for a in $(cat b); do
-  echo " * $a"
-  wget -c $a 2>&1 | grep already
+  f=$(basename $a)
+  if [ ! -f $a ]; then
+    echo " * $a"
+    rm -rf tmp
+    mkdir -p tmp
+    cd tmp
+    wget -q $a 2>&1
+    mv * ..
+    cd ..
+  fi
 done
-eog . & #gqview &

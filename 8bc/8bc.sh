a65db487c42b5c8a9369d0dbe8791304d5e52bbe
pancake <pancake@nopcode.org>
2011-06-16 11:42:35 +0200

    * Fix 8bc.sh

--- 8bc/8bc.sh
+++ 8bc/8bc.sh
@@ -2,7 +2,7 @@
 # 8bc - GPL3 - nibble <develsec.org> 2009
 # Play 8BitCollective stream
 
-wget -q -O - http://www.8bitcollective.com/rss/music | \
-grep "http://www.8bitcollective.com/items/music/" | \
+wget -q -O - http://8bc.org/rss/music | \
+grep "http://www.8bc.org/items/music/" | \
 sed 's/.*\(http.*\)" l.*/\1/' > /tmp/8bc.pls
 mplayer -playlist /tmp/8bc.pls
